81d83462b86959cc08a22827a301350c870bec4d
ajonescode <54915239+ajonescode@users.noreply.github.com>
2022-10-27 14:55:11 -0500

    Player Details added to Player

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PlayerDetails from './PlayerDetails'
 
 const Player = (props) =>{
   return(
     <div className="c-player">
       <audio></audio>
       <h4>Playing now</h4>
-      {/* Details */}
+      <PlayerDetails song = {props.song} />
       {/* Controls */}
       <p><strong>Next up:</strong> {props.nextSong.title} by {props.nextSong.artist}</p>
     </div>
--- src/Components/PlayerDetails.js
+++ src/Components/PlayerDetails.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 
-const PlayerDetails = () => {
+const PlayerDetails = (props) => {
   return(
 <div className="c-player--details">
 <div className="details-img">
-  <img src = "" alt ="" />
+  <img src = {props.song.img_src} alt ="" />
 </div>
 <h3 className="details-title">
-
+    {props.song.title}
 </h3>
 <h4 className="details-artist">
-  
+    {props.song.artist}
 </h4>
 </div>
   );
