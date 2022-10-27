d87e47095fbeea7deb1e6f55c28ed1969483cf4a
ajonescode <54915239+ajonescode@users.noreply.github.com>
2022-10-27 15:41:03 -0500

    1 file modified

--- src/Components/PlayerControls.js
+++ src/Components/PlayerControls.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-svg-icons'
 
-const PlayerControls = () =>{
+const PlayerControls = (props) =>{
 
 return(
   <div className="c-player--controls">
-    <button className="skip-btn">
-      <FontAwesomeIcon icon = {faBackward} />
-    </button>
-    <button className="play-btn">
-      <FontAwesomeIcon icon = {faPlay} />
-    </button>
-    <button className="skip-btn">
-      <FontAwesomeIcon icon = {faForward} />
-    </button>
-  </div>
+  <button className="skip-btn" onClick={() => props.SkipSong(false)}>
+      <FontAwesomeIcon icon={faBackward} />
+  </button>
+  <button className="play-btn" onClick={() => props.setIsPlaying(!props.isPlaying)}>
+      <FontAwesomeIcon icon={props.isPlaying ? faPause : faPlay} />
+  </button>
+  <button className="skip-btn" onClick={() => props.SkipSong()}>
+      <FontAwesomeIcon icon={faForward} />
+  </button>
+</div>
 
 );
 }
