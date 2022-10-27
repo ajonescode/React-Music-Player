f779caf2234b63ee0c80e01f4ebed6ec850f9e28
ajonescode <54915239+ajonescode@users.noreply.github.com>
2022-10-27 15:18:34 -0500

    2 files modified

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -8,7 +8,8 @@ const Player = (props) =>{
       <audio></audio>
       <h4>Playing now</h4>
       <PlayerDetails song = {props.song} />
-      
+      <PlayerControls />
+
      
       <p><strong>Next up:</strong> {props.nextSong.title} by {props.nextSong.artist}</p>
     </div>
