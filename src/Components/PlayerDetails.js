a86d0790a9fb6ea64dcf0cab4dcc8f708fde7d2c
ajonescode <54915239+ajonescode@users.noreply.github.com>
2022-10-27 14:50:21 -0500

    player details update

--- src/Components/PlayerDetails.js
+++ src/Components/PlayerDetails.js
@@ -3,7 +3,17 @@ import React from 'react';
 
 const PlayerDetails = () => {
   return(
+<div className="c-player--details">
+<div className="details-img">
+  <img src = "" alt ="" />
+</div>
+<h3 className="details-title">
 
+</h3>
+<h4 className="details-artist">
+  
+</h4>
+</div>
   );
 }
 
